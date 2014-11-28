708517850392793bc52d3eb0973bfe0ff487b458
Thorrik58 <Thorrik58@gmail.com>
2014-11-28 16:16:11 +0000

    docker exports added

--- dockerbuild.sh
+++ dockerbuild.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+
+
 echo Cleaning...
 rm -rf ./dist
 
@@ -19,6 +21,11 @@ cd dist
 npm install --production
 
 echo Building docker image
+
+export DOCKER_HOST=tcp://192.168.59.103:2376
+export DOCKER_CERT_PATH=/Users/Thorri/.boot2docker/certs/boot2docker-vm
+export DOCKER_TLS_VERIFY=1
+
 docker build -t thorrik58/tictactoe .
 
 echo "Done"
