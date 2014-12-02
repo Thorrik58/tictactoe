28910633f1e440d5db5736aae7fed3df8e6d99ba
Thorrik58 <Thorrik58@gmail.com>
2014-12-02 14:36:06 +0000

    Update dockerbuild.sh
    docker now running on production, i hope.....

--- dockerbuild.sh
+++ dockerbuild.sh
@@ -22,9 +22,7 @@ npm install --production
 
 echo Building docker image
 
-export DOCKER_HOST=tcp://192.168.59.103:2376
-export DOCKER_CERT_PATH=/Users/Thorri/.boot2docker/certs/boot2docker-vm
-export DOCKER_TLS_VERIFY=1
+docker run -p 80:8080 -d -e "NODE_ENV=production" thorrik58/tictactoe
 
 docker build -t thorrik58/tictactoe .
 
