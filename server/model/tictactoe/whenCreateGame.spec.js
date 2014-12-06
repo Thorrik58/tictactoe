1ec587411da31483f351aa288581afacb9a2b3bf
Þorvarður Örn Einarsson <Thorri@orvarurs-MacBook-Pro.local>
2014-12-06 16:21:30 +0000

    CreateGame test implemented and passed

--- server/model/tictactoe/whenCreateGame.spec.js
+++ server/model/tictactoe/whenCreateGame.spec.js
@@ -3,3 +3,32 @@ var _ = require('lodash');
 
 var tictactoe = require('./tictactoe')
 
+describe("Create game command", function()
+{
+  it('Should emit game created event', function(){
+
+    var given = [];
+
+    var when = {
+      cmd: "CreateGame",
+      user: {
+        userName: "Thorri"
+      },
+      name: "TheFirstGame",
+      timeStamp: "2014-12-02T11:29:29"
+    };
+
+    var then = [{
+      event: "GameCreated",
+      user: {
+        userName: "Thorri"
+      },
+      name: "TheFirstGame",
+      timeStamp: "2014-12-02T11:29:29"
+    }];
+
+    var actualEvents = tictactoe(given).executeCommand(when);
+    should(actualEvents.length).be.exactly(1);
+    should(JSON.stringify(actualEvents)).be.exactly(JSON.stringify(then));
+  })
+})
