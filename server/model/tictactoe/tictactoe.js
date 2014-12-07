6ae77a2076780155225b1dc4bb1ab5c661c752c2
Þorvarður Örn Einarsson <Thorri@orvarurs-MacBook-Pro.local>
2014-12-07 21:41:40 +0000

    TicTacToe functions PlaceMove implemented

--- server/model/tictactoe/tictactoe.js
+++ server/model/tictactoe/tictactoe.js
@@ -18,10 +18,10 @@ module.exports = function(history){
             timeStamp: cmd.timeStamp
           }]
         },
-        "JoinGame": function(cmd){
-          if(gameState.gameFull()){
+        "JoinGame": function (cmd) {
+          if (gameState.gameFull()) {
             console.log("Game full");
-            return[{
+            return [{
               event: "FullGameJoinAttempted",
               user: cmd.user,
               name: cmd.name,
@@ -29,16 +29,47 @@ module.exports = function(history){
             }];
           }
 
-          console.log("Join game");
           gameFull = true;
-          return[{
+          return [{
             event: "GameJoined",
             user: cmd.user,
             name: cmd.name,
             timeStamp: cmd.timeStamp
           }];
+        },
+
+        "PlaceMove": function (cmd) {
+          if (gameState.occupied(cmd.move.coordinates)) {
+            return [{
+              event: "IllegalMove",
+              user: cmd.user,
+              name: cmd.name,
+              timeStamp: cmd.timeStamp,
+              move: cmd.move
+            }]
+          }
+          var events = [{
+            event: "MovePlaced",
+            user: cmd.user,
+            name: cmd.name,
+            timeStamp: cmd.timeStamp,
+            move: cmd.move
+          }];
+
+          gamestate.processEvents(events);
+          if (gameState.gameWon()) {
+            events.push(
+              {
+                event: "GameWon",
+                user: cmd.user,
+                name: cmd.name,
+                timeStamp: cmd.timeStamp
+              }
+            )
+          }
+          return events;
         }
-      }
+      };
       return cmdHandlers[cmd.cmd](cmd);
     }
   }
--- server/model/tictactoe/tictactoeState.js
+++ server/model/tictactoe/tictactoeState.js
@@ -5,11 +5,13 @@ var _ = require('lodash');
 
 module.exports = function(history){
   var gameFull = false;
+
   _.each(history, function(event){
     if(event.event === "GameJoined"){
       gameFull = true;
     }
   });
+
   return {
     gameFull : function(){
       return gameFull;
