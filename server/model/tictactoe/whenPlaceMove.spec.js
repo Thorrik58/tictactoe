238c2977742f3670b14102cbfff31de99da55782
Þorvarður Örn Einarsson <Thorri@orvarurs-MacBook-Pro.local>
2014-12-08 13:51:47 +0000

    Move placed test done

---
+++ server/model/tictactoe/whenPlaceMove.spec.js
@@ -0,0 +1,67 @@
+var should = require('should');
+var _ = require('lodash');
+
+var tictactoe = require('./tictactoe')
+
+var createEvent = {
+  event: "GameCreated",
+  user: {
+    userName: "Thorri"
+  },
+  name: "TheFirstGame",
+  timeStamp: "2014-12-02T11:29:29"
+};
+
+var joinEvent = {
+  event:"EventJoined",
+  user:{
+    userName: "Max"
+  },
+  name: "TheFirstGame",
+  timeStamp: "2014-12-02T11:29:29"
+};
+
+function moveEvent(coordinates, side){
+  return{
+    event: "movePlaced",
+    user:{
+      userName: "Max"
+    },
+    name: "TheFirstGame",
+    timeStamp: "2014-12-02T11:29:29",
+    move: {
+      coordinates: coordinates,
+      side: side
+    }
+  };
+}
+
+
+describe('place move command', function() {
+  var given, when, then;
+
+  afterEach(function(){
+    var actualEvents = tictactoe(given).executeCommand(when);
+    should(JSON.stringify(actualEvents)).be.exactly(JSON.stringify(then))
+  });
+
+  it('should emit place move event'), function(){
+    given = [createEvent, joinEvent];
+
+    when = {
+      cmd: "PlaceMove",
+      user: {
+        userName: Max
+      },
+      name: "TheFirstGame",
+      timeStamp: "2014-12-02T11:29:29",
+      move: {
+        coordinates: [0, 0],
+        side: 'X'
+      }
+    };
+
+    then = [moveEvent([0,0], 'X')];
+  }
+
+});
