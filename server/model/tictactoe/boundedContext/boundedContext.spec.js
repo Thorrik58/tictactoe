b8cc7dc28689bd63194e367ce7982c3b5a43809a
Þorvarður Örn Einarsson <Thorri@orvarurs-MacBook-Pro.local>
2014-12-15 16:05:56 +0000

    BoundedContext test, using jsmockito

--- server/model/tictactoe/boundedContext/boundedContext.spec.js
+++ server/model/tictactoe/boundedContext/boundedContext.spec.js
@@ -10,11 +10,12 @@ describe('tictactoe game context using stub', function() {
       }
     };
 
-    var execCmd = {};
+    var executedCommand = {};
+
     var tictactoe = function(history){
       return{
         executeCommand : function(cmd){
-          executeCommand = cmd;
+          executedCommand = cmd;
           return [];
         }
       }
@@ -29,7 +30,45 @@ describe('tictactoe game context using stub', function() {
     };
 
     var events = gameContext.handleCmd(testCmd);
-    should(executeCommand.id).be.exactly("123")
+    should(executedCommand.id).be.exactly("123")
     should(events.length).be.exactly(0);
+  })
+
+  it('should route command to instantiated tictactoe game with event stream from store and return generated events, using mock style tests.', function(){
+
+    var jm = require('jsmockito').JsMockito;
+
+    jm.Integration.importTo(global);
+
+    var mockStore = spy({
+      loadEvents : function(){
+    }
+    });
+
+    when(mockStore).loadEvents('123').thenReturn([]);
+
+    var mockTicTacToe = spy({
+      executeCommand : function(){
+      }
+    });
+
+    when(mockTicTacToe).executeCommand().thenReturn([]);
+
+    var commandHandlers = [function(){
+      return mockTicTacToe
+    }];
+
+    var gameContext = require('./boundedContext')(mockStore, commandHandlers);
+
+    var testCmd = {
+      id: "123"
+    };
+
+    gameContext.handleCmd(testCmd);
+
+    jm.verify(mockStore).loadEvents('123');
+
+    jm.verify(mockTicTacToe).executeCommand(testCmd);
+
   })
 })
