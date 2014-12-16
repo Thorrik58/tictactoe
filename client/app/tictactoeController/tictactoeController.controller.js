c189cca6dc23584ea61e5d448f443cb048d62415
Þorvarður Örn Einarsson <Thorri@orvarurs-MacBook-Pro.local>
2014-12-16 23:27:32 +0000

    flush fail again, need to fix, e2e improved

--- client/app/tictactoeController/tictactoeController.controller.js
+++ client/app/tictactoeController/tictactoeController.controller.js
@@ -33,7 +33,7 @@ angular.module('tictactoeApp')
     }
 
     refresh();
-    $interval(refresh, 1000);
+    $interval(refresh, 2000);
 
     function mySide() {
       return $location.search()['gameSide'];
--- e2e/main/tictactoe.po.js
+++ e2e/main/tictactoe.po.js
@@ -5,10 +5,29 @@
 
 'use strict';
 
+var _ = require('lodash');
+
 var TictactoePage = function() {
   this.container = element(by.css('.container'));
   this.board = this.container.element(by.css('.board'));
+  this.joinLink = this.container.element(by.css('#joinlink'));
+  this.winner = this.container.element(by.css('.winner'));
+
+  this.opponentname = this.container.element(by.css('#opponentname'));
+  this.myname = this.container.element(by.css('#myname'));
+
   this.x0y0 = this.board.element(by.css('.x0y0'));
+  this.x0y1 = this.board.element(by.css('.x0y1'));
+  this.x0y2 = this.board.element(by.css('.x0y2'));
+
+  this.x1y0 = this.board.element(by.css('.x1y0'));
+  this.x1y1 = this.board.element(by.css('.x1y1'));
+  this.x1y2 = this.board.element(by.css('.x1y2'));
+
+  this.x2y0 = this.board.element(by.css('.x2y0'));
+  this.x2y1 = this.board.element(by.css('.x2y1'));
+  this.x2y2 = this.board.element(by.css('.x2y2'));
+
 };
 
 module.exports = new TictactoePage();
